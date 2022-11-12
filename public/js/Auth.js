e64b063aa1c8f6e97be84e98ce64f3c399ff8b95
Afzaal  Iqbal <afzaalwurfelit@gmail.com>
2022-11-12 14:27:41 +0500

    login api integrated

--- public/js/Auth.js
+++ public/js/Auth.js
@@ -1,4 +1,52 @@
-function login(e){
+let baseurlBackend = "http://localhost:7000/api";
+function loginForm(e){
 e.preventDefault();
-alert('testing here')
+const formData = new FormData(document.querySelector('#login'));
+// alert('testing here');
+$.ajax({
+    url: baseurlBackend+"/login",
+    // headers: {  'Access-Control-Allow-Origin': '*' },
+    // crossDomain: true,
+    type: 'POST',
+    data: formData,
+    dataType: "json",
+    contentType: false,
+    cache: false,
+    processData: false,
+    //THIS NEEDS TO BE GET
+    success: function success(response) {
+        localStorage.setItem('jwt_token',response.token);  
+      console.log('login response for debugging', response.message);
+      if(response.success == false){
+      $('.loginFormAlertDiv').css('display','');
+      $('.loginFormAlertDiv').append(response.message);
+      }
+    //   messageToaster('success', response.success, 'Success');
+      setTimeout(() => {
+        $('.loginFormAlertDiv').css('display','none');
+        $('.loginFormAlertDiv').empty();
+        // $('#login-page').modal("hide");
+        $('.login-submit-btn').text('CONTINUE');
+      }, 2500);
+      if(response.success==true){
+        $('.loginFormSuccessAlertDiv').css('display','');
+        $('.loginFormSuccessAlertDiv').append(response.message);
+        setTimeout(() => {
+          $('.loginFormSuccessAlertDiv').css('display','none');
+          $('.loginFormSuccessAlertDiv').empty();
+          if(response.success == true){
+        //   window.location = '/admin';
+          }else{
+            // window.location = '/admin/search-business';
+          }
+          $('.login-submit-btn').text('CONTINUE');
+        }, 1500);
+      }
+
+    },
+    error: function error(error) {
+      console.log('login error accoured',error);
+      $('.login-submit-btn').text('CONTINUE');
+    }
+  });
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,16 +43,18 @@
       <div class="container">
         <div class="row">
           <div class="col-md-4 col-md-offset-4">
+          <div class="alert alert-success  loginFormAlertDiv" style="display:none"
+                                        role="alert"></div>
             <form id="login" action="" class="well">
                   <div class="form-group">
                     <label>Email Address</label>
-                    <input type="text" class="form-control" placeholder="Enter Email">
+                    <input type="text" class="form-control" name="email" id="id" placeholder="Enter Email">
                   </div>
                   <div class="form-group">
                     <label>Password</label>
-                    <input type="password" class="form-control" placeholder="Password">
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Password">
                   </div>
-                  <button onclick="login(event);" type="submit" class="btn btn-default btn-block">Login</button>
+                  <button onclick="loginForm(event);" type="submit" class="btn btn-default btn-block">Login</button>
               </form>
           </div>
         </div>
